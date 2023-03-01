6d350664d2e84a8ae9df5dc6f101e61c9e7b9119
Jarl André Hübenthal <jarlah@protonmail.com>
2023-03-01 20:00:06 +0100

    unit tests

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 ## How to call the agreement api
 
 ```
-~  curl -X POST -d '{ "customerPid": "11233", "customerName": "Jarl", "agreementPrice": 123.233 }' -H "Content-Type: application/json" http://localhost:8080/api/agreement
+~  curl -X POST -d '{ "customerPid": "11233", "customerName": "Jarl", "agreementPrice": 123.233 }' -H "Content-Type: application/json" -H "X-Correlation-Id: a3dfbdf3-3fae-40e2-aa6e-de76c1220f65" http://localhost:8080/api/agreement
 {"id":"1414eef5-dcfd-4b4d-9c98-f07e1ef1bab4","agreementPrice":123.233,"customerId":"6a1dfe10-fbf7-4776-aab6-95fb9b3708c4"}%
 ~ 
 ```
---
+++ src/test/java/com/example/demo/IntegrationControllerTest.java
@@ -0,0 +1,77 @@
+package com.example.demo;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+public class IntegrationControllerTest {
+
+  @Autowired private TestRestTemplate restTemplate;
+
+  @Test
+  public void createAgreementWithValidPayload() {
+    Map<String, Object> obj = new HashMap<>();
+    obj.put("customerPid", "11111111111");
+    obj.put("customerName", "Donald Duck");
+    obj.put("agreementPrice", 1000);
+    ResponseEntity<String> entity = this.restTemplate.postForEntity("/api/agreement", obj, String.class);
+    assertThat(entity.getStatusCode()).isEqualTo(HttpStatus.OK);
+    // TODO find a way to assert json without resorting to POJOs
+  }
+
+  @Test
+  public void createAgreementWithBadCustomerPid() {
+    Map<String, Object> obj = new HashMap<>();
+    obj.put("customerPid", "111111111111111111");
+    obj.put("customerName", "Donald Duck");
+    obj.put("agreementPrice", 1000);
+    ResponseEntity<String> entity = this.restTemplate.postForEntity("/api/agreement", obj, String.class);
+    assertThat(entity.getStatusCode()).isEqualTo(HttpStatus.BAD_REQUEST);
+    assertThat(entity.getBody())
+        .isEqualTo(
+            "{\"errors\":[{\"field\":\"createAgreement.newAgreementDto.customerPid\","
+                + "\"message\":\"size must be between 1 and 11\"}]}");
+  }
+
+  @Test
+  public void createAgreementWithBadAgreementPrice() {
+    Map<String, Object> obj = new HashMap<>();
+    obj.put("customerPid", "01019912345");
+    obj.put("customerName", "Donald Duck");
+    obj.put("agreementPrice", 0);
+    ResponseEntity<String> entity = this.restTemplate.postForEntity("/api/agreement", obj, String.class);
+    assertThat(entity.getStatusCode()).isEqualTo(HttpStatus.BAD_REQUEST);
+    assertThat(entity.getBody())
+        .isEqualTo(
+            "{\"errors\":[{\"field\":\"createAgreement.newAgreementDto.agreementPrice\","
+                + "\"message\":\"must be greater than or equal to 1\"}]}");
+  }
+
+  @Test
+  public void createAgreementWithBadCustomerName() {
+    Map<String, Object> obj = new HashMap<>();
+    obj.put("customerPid", "01019912345");
+    obj.put("customerName", "");
+    obj.put("agreementPrice", 1000);
+    ResponseEntity<String> entity = this.restTemplate.postForEntity("/api/agreement", obj, String.class);
+    assertThat(entity.getStatusCode()).isEqualTo(HttpStatus.BAD_REQUEST);
+    assertThat(entity.getBody())
+        .isEqualTo(
+            "{\"errors\":[{\"field\":\"createAgreement.newAgreementDto.customerName\","
+                + "\"message\":\"size must be between 1 and 100\"}]}");
+  }
+}
