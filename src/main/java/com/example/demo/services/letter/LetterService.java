ab7ec06fa5a039301656682d1af2003be3e4449e
Jarl André Hübenthal <jarlah@protonmail.com>
2023-03-04 22:02:05 +0100

    readme

--- src/main/java/com/example/demo/services/letter/LetterService.java
+++ src/main/java/com/example/demo/services/letter/LetterService.java
@@ -5,6 +5,7 @@ import com.example.demo.services.business.models.Customer;
 import com.example.demo.services.letter.exceptions.LetterFailedException;
 import com.example.demo.services.letter.models.LetterStatus;
 
+/** This service might call out to an external api */
 public interface LetterService {
   LetterStatus sendAgreementLetterToCustomer(Agreement agreement, Customer customer)
       throws LetterFailedException;
